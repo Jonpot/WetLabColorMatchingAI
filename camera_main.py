ef956f6866492f2d292b35d5820cc385f365d38c
Chris0810 <74512891+shicheng0810@users.noreply.github.com>
2025-04-14 15:37:46 -0400

    Update camera_main.py

--- camera_main.py
+++ camera_main.py
@@ -3,19 +3,6 @@ from ot2_utils import OT2Manager
 from camera_process import CameraImageProcessor
 
 def main():
-    # # Connect to the OT-2 robot
-    # print("Connecting to OT2 robot...")
-    # robot = OT2Manager(
-    #     hostname="172.26.192.201",
-    #     username="root",
-    #     key_filename="C:/Users/shich/.ssh/ot2_ssh_key",
-    #     password=""
-    # )
-    
-    # # Add a blink lights action to the robot and execute it remotely
-    # robot.add_blink_lights_action(num_blinks=5)
-    # robot.execute_actions_on_remote()
-    
     # Create an instance of the camera processor to capture and process an image
     camera = CameraImageProcessor()
     
@@ -45,10 +32,6 @@ def main():
         print(rgb_matrix)
         rgb_stats = camera.compute_rgb_statistics(rgb_matrix)
         print("RGB Statistics:", rgb_stats)
-    
-    # # Add a close action (if your workflow requires closing the session on the OT-2)
-    # robot.add_close_action()
-    # robot.execute_actions_on_remote()
 
 if __name__ == "__main__":
     main()
