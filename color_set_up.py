0baed685e40b5e48101133ce8a35658730f91b03
Chris0810 <lsc2018@qq.com>
2025-05-06 11:48:26 -0400

    update

--- color_set_up.py
+++ color_set_up.py
@@ -11,24 +11,44 @@ except Exception as e:
 plate_col_letters = ["1", "2", "3", "4", "5", "6","7", "8", "9", "10", "11", "12"]
 plate_rows_letters = ["A", "B", "C", "D", "E", "F", "G","H"]
 
+# Split row letters into upper and lower halves
+upper_rows = plate_rows_letters[:4]  # ['A', 'B', 'C', 'D']
+lower_rows = plate_rows_letters[4:]  # ['E', 'F', 'G', 'H']
+
+# Generate full well IDs
+upper_half = [row + col for col in plate_col_letters for row in upper_rows]
+upper_half_shuffle = np.random.shuffle(upper_half)
+# lower_half = [row + col for col in plate_col_letters for row in lower_rows]
+
 color_slots = ["7", "8", "9", "11"] # red green blue water
-volume = [200, 180, 120, 80, 20]
+volumne_list = [200, 180, 120, 80, 20, 0]
+volume_pair = [(v, 0) for v in volumne_list[:-1]]
+color_pair = [(c1, c2) for c1 in color_slots for c2 in color_slots if c1 != c2]
+
+# robot.add_turn_on_lights_action()
+# Combine color pairs and volumes, allowing mirrored pairs
+color_volume_pairs = []
+for i, c1 in enumerate(color_slots):
+    for j, c2 in enumerate(color_slots):
+        if i != j:
+            v1, v2 = volume_pair[i]
+            color_volume_pairs.append((c1, c2, v1, v2))
+color_volume_pairs = 4*color_volume_pairs
+
+# Combine with shuffled upper_half wells
+combined = list(zip(upper_half, color_volume_pairs))
+
+# Final format: (well, color1, color2, vol1, vol2)
+combined = [(well, c1, c2, v1, v2) for well, (c1, c2, v1, v2) in combined]
+print(combined)
+
+for i in combined:
+    robot.add_add_color_action(color_slot=i[1], plate_well=i[0], volume=i[3])
+    robot.add_add_color_action(color_slot=i[2], plate_well=i[0], volume=i[4])
+    robot.execute_actions_on_remote()
 
 
-
-# np.shuffle
-
-robot.add_turn_on_lights_action()
-robot.add_add_color_action(color_slot='7', plate_well="A1", volume=30)
-robot.add_add_color_action(color_slot='8', plate_well="A2", volume=30)
-robot.add_add_color_action(color_slot='7', plate_well="A1", volume=30)
-robot.add_add_color_action(color_slot='8', plate_well="A2", volume=30)
-robot.add_add_color_action(color_slot='9', plate_well="A3", volume=30)
-robot.execute_actions_on_remote()
-
-#robot.add_blink_lights_action(num_blinks=5)
 robot.add_turn_off_lights_action()
-# robot.add_add_color_action(color_slot='7', plate_well="A1", volume=30)
 robot.add_close_action()
-
 robot.execute_actions_on_remote()
+
